b493c4fed3dd980838221840c8981e5b06a22028
CsVazquezz <cmmv1971@outlook.com>
2025-03-11 10:09:23 -0600

    comments: app.js (lab11)

--- app.js
+++ app.js
@@ -2,31 +2,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+// Import routes
 const productsRoutes = require("./routes/products.routes");
 const usersRoutes = require("./routes/users.routes");
 const mainRoutes = require("./routes/main.routes");
 
-// Middleware
+// Middleware to log requests
 app.use((request, response, next) => {
   console.log("Middleware!");
-  next(); // Le permite a la petición avanzar hacia el siguiente middleware
+  next(); // Send petition to go to next middleware
 });
 
+// Middleware to send a response
 app.use((request, response, next) => {
   console.log("Otro middleware!");
-  response.send("¡Hola mundo!"); // Manda la respuesta
+  response.send("¡Hola mundo!"); // Send the response
 });
 
+// Middleware to parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/products", productsRoutes);
-app.use("/users", usersRoutes);
-app.use("/", mainRoutes);
+// Routes
+app.use("/products", productsRoutes); // Routes for products
+app.use("/users", usersRoutes); // Routes for users
+app.use("/", mainRoutes); // Main routes
 
+// Middleware to handle 404 errors
 app.use((request, response, next) => {
   response.status(404).send("Página no encontrada");
 });
 
+// Start the server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
